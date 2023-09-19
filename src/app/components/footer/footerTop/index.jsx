665659998af11a4cf4c44790c15e224b5a4166e8
ArshiaXII <82704919+ArshiaXII@users.noreply.github.com>
2023-09-19 22:18:38 +0300

    Update index.jsx

--- src/app/components/footer/footerTop/index.jsx
+++ src/app/components/footer/footerTop/index.jsx
@@ -1,5 +1,6 @@
-import S from './YourStylesheet.module.css'; // Siz ne kullanıyorsanız onu import edin.
-import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'; // Eğer React Icons kullanıyorsanız
+import React from 'react';
+import * as S from "./styled";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
 export default function FooterTop() {
     return (
@@ -8,24 +9,31 @@ export default function FooterTop() {
                 <div className={S.col}>
                     <div className={S.title}>SOSYAL MEDYA</div>
                     <div className={S.medias}>
-                        <a href="https://www.instagram.com/fpsbilgisayar/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/fpsbilgisayar/" target="_blank" rel="noreferrer">
                             <div className={S.icon}>
                                 <FiFacebook />
                             </div>
                         </a>
-                        <a href="https://www.instagram.com/fpsbilgisayar/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/fpsbilgisayar/" target="_blank" rel="noreferrer">
                             <div className={S.icon}>
                                 <FiTwitter />
                             </div>
                         </a>
-                        <a href="https://www.instagram.com/fpsbilgisayar/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/fpsbilgisayar/" target="_blank" rel="noreferrer">
                             <div className={S.icon}>
                                 <FiInstagram />
                             </div>
                         </a>
                     </div>
                 </div>
+                <div className={S.col}>
+                    <div className={S.title}>E-BÜLTEN</div>
+                    <div>
+                        <input className={S.input} type="text" />
+                        <button className={S.btn}>EKLE</button>
+                    </div>
+                </div>
             </div>
         </div>
-    );
+    )
 }
